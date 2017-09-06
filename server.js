eb66b2f9b0a3d9817947bc7407afe2df9b571b08
Stephen Boynton <boyntonsc@gmail.com>
2017-09-06 16:12:05 -0400

    Add .env file and setup login authentication

---
+++ server.js
@@ -0,0 +1,50 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const mustacheExpress = require("mustache-express");
+const mongoose = require("mongoose");
+const { User, Message } = require("./models/Schema");
+
+app.engine("mustache", mustacheExpress());
+app.set("view engine", "mustache");
+app.set("views", __dirname + "/views");
+
+app.use(express.static("public"));
+app.use(
+	session({
+		secret: process.env.SESSION_SECRET,
+		saveUninitialized: false,
+		resave: false
+	})
+);
+
+function valdiateLogin (req, res, next) {
+  getUserByUserName(req.body.username).then((user) => {
+    user.validPassword(req.body.password, user.password, (err, isMatch) {
+      if(err) {
+        req.session.message = "Invalid Username or Password."
+        res.redirect("/login", req.session.message)
+      } else if (isMatch) {
+        req.session.user = {
+          username: user.username,
+          name: user.name,
+          avatar: user.avatar,
+          userId: user._id
+        };
+        next();
+      }
+    })
+  })
+}
+
+app.post("/login", validateLogin, (req, res) => {
+	res.redirect("/inbox");
+});
+
+app.set("port", 3000);
+
+app.listen(app.get("port"), () => {
+	console.log("Your app has started, sir.");
+});
