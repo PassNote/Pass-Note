982e2c4daa9066c0f90f33043ca272369dc611d9
Stephen Boynton <boyntonsc@gmail.com>
2017-09-06 15:42:38 -0400

    Create User and Message Schemas

---
+++ model/Schemas.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const moment = require("moment");
+const bcrypt = require("bcrypt");
+
+const UserSchema = new mongoose.Schema({
+	username: { type: String },
+	password: { type: String },
+	avatar: {
+		type: String,
+		default: "https://openclipart.org/download/247316/abstract-user-flat-2.svg"
+	},
+	name: { type: String },
+	contacts: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+	incoming: [{ type: mongoose.Schema.Types.ObjectId, ref: "Message" }],
+	outgoing: [{ type: mongoose.Schema.Types.ObjectId, ref: "Message" }]
+});
+
+const MessageSchema = new mongoose.Schema({
+	title: { type: String },
+	body: { type: String },
+	date: { type: Date, default: moment().format() },
+	sender: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+	recipient: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }]
+});
+
+UserSchema.statics.generateHash = function(password) {
+	return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+// checking if password is valid
+UserSchema.methods.validPassword = function(password, dbpassword, done) {
+	bcrypt.compare(password, dbpassword, (err, isMatch) => {
+		console.log("password check");
+		done(err, isMatch);
+	});
+};
+
+const User = mongoose.model("User", UserSchema);
+const Message = mongoose.model("Message", MessageSchema);
+
+module.exports = { User, Message };
