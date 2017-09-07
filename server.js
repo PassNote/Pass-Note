d40d5daed5a808d9dd1677e99edcdfd7ea9d7d1a
Stephen Boynton <boyntonsc@gmail.com>
2017-09-07 08:27:36 -0400

    Minor cleanup to get the server running

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const session = require("express-session");
 const mustacheExpress = require("mustache-express");
 const mongoose = require("mongoose");
-const { User, Message } = require("./models/Schema");
+const { User, Message } = require("./model/Schemas");
 
 app.engine("mustache", mustacheExpress());
 app.set("view engine", "mustache");
@@ -20,48 +20,52 @@ app.use(
 	})
 );
 
-function valdiateLogin (req, res, next) {
-  getUserByUserName(req.body.username).then((user) => {
-    user.validPassword(req.body.password, user.password, (err, isMatch) => {
-      if(err) {
-        req.session.message = "Invalid Username or Password."
-        res.redirect("/login", req.session.message)
-      } else if (isMatch) {
-        req.session.user = {
-          username: user.username,
-          name: user.name,
-          avatar: user.avatar,
-          userId: user._id
-        };
-        next();
-      }
-    })
-  })
+function validateLogin(req, res, next) {
+	getUserByUserName(req.body.username).then(user => {
+		user.validPassword(req.body.password, user.password, (err, isMatch) => {
+			if (err) {
+				req.session.message = "Invalid Username or Password.";
+				res.redirect("/login", req.session.message);
+			} else if (isMatch) {
+				req.session.user = {
+					username: user.username,
+					name: user.name,
+					avatar: user.avatar,
+					userId: user._id
+				};
+				next();
+			}
+		});
+	});
 }
 
-app.get('/', function (req, res){
-    res.redirect('/login')
+app.get("/", function(req, res) {
+	res.redirect("/login");
+});
+
+app.get("/login", (req, res) => {
+	res.render("login");
 });
 
 app.post("/login", validateLogin, (req, res) => {
-    res.redirect("/inbox");
+	res.redirect("/inbox");
 });
 
-app.get ('signup', function(req, res){
-    res.render('signup')
-})
+app.get("/signup", function(req, res) {
+	res.render("signup");
+});
 
-app.post('/signup', function(req, res){
-    res.redirect('/inbox')
-})
+app.post("/signup", function(req, res) {
+	res.redirect("/inbox");
+});
 
-app.get('compose', function(req, res){
-    res.render('compose')
-})
+app.get("/compose", function(req, res) {
+	res.render("compose");
+});
 
-app.post('/compose', function(req, res){
-    res.redirect('/inbox')
-})
+app.post("/compose", function(req, res) {
+	res.redirect("/inbox");
+});
 
 app.set("port", 3000);
 
